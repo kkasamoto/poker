766f0410195acafbe7a465d54c3ae8784ff3dc09
kkasamoto <kasamoto.k777@gmail.com>
2019-07-24 11:43:27 +0900

    [fix] change redirect to render of check action

--- app/controllers/poker_controller.rb
+++ app/controllers/poker_controller.rb
@@ -9,11 +9,12 @@ class PokerController < ApplicationController
     service = Service.new
     result = service.hand_name(card_str: params[:card])
     if result[:has_error]
-      flash[:err_messages] = result[:err_messages]
+      flash.now[:err_messages] = result[:err_messages]
       session[:hand_name] = nil
+      render('poker/top', status: 400)
     else
       session[:hand_name] = result[:hand_name]
+      render('poker/top', status: 200)
     end
-    redirect_to("/")
   end
 end
