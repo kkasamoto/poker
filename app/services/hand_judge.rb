686bdbcd231b6e4fb75c418bc59b88822c633c59
kkasamoto <kasamoto.k777@gmail.com>
2019-07-24 15:36:19 +0900

    [mod] remove card_validator.rb and valid integrate with hand_judge

--- app/services/hand_judge.rb
+++ app/services/hand_judge.rb
@@ -1,5 +1,6 @@
 class HandJudge
   attr_reader :card
+  attr_reader :err_messages
 
   module Hands
     STRAIGHT_FLUSH="ストレートフラッシュ"
@@ -27,6 +28,37 @@ class HandJudge
 
   def initialize(card)
     @card = card
+    @err_messages = []
+  end
+
+  # Arguments:
+  #   card(array<str>):  トランプ文字列5つからなる配列
+  def valid?
+    # 1. cardは5枚あるか？
+    # 2. cardそれぞれは指定のフォーマットを満たしているか？
+    # 3. cardに重複はないか？
+
+    # 1.
+    if @card.length != 5
+      @err_messages.append('5つのカード指定文字を半角スペース区切りで入力してください。（例："S1 H3 D9 C13 S11"）')
+      return false
+    end
+    #2.
+    @card.each_with_index  do |c,idx|
+      if not c.match(/^[SHDC](\d|10|11|12|13)$/)
+        @err_messages.append("#{idx+1}番目のカード指定文字が不正です。（#{c}）")
+      end
+    end
+    if @err_messages.length >= 1
+      @err_messages.append('半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。')
+      return false
+    end
+    #3.
+    if @card.length != @card.uniq.length
+      @err_messages.append('カードが重複しています。')
+      return false
+    end
+    true
   end
 
   # 役の強さを返す
--- app/services/poker_facade_service.rb
+++ app/services/poker_facade_service.rb
@@ -1,12 +1,11 @@
 class PokerFacadeService
   def hand_name(card_str:)
     card = card_str.strip.split
-    validator = CardValidator.new
     hand_judge = HandJudge.new(card)
-    if validator.valid?(card)
+    if hand_judge.valid?
       {hand_name: hand_judge.judge_name, err_messages: [], has_error: false}
     else
-      {hand_name: '', err_messages: validator.err_messages, has_error: true}
+      {hand_name: '', err_messages: hand_judge.err_messages, has_error: true}
     end
   end
 end
