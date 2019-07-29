89b66ec3a2f72ed1137536c1df18266087f86414
kkasamoto <kasamoto.k777@gmail.com>
2019-07-29 13:18:09 +0900

    [add] finish core api

--- app/apis/resources/v1/cards.rb
+++ app/apis/resources/v1/cards.rb
@@ -2,7 +2,10 @@ module Resources
   module V1
     class Cards < Grape::API
       resources :cards do
+        desc "check which card is winner"
         post :check do
+          service = PokerFacadeService.new
+          service.which_strong_card(params[:cards])
         end
       end
     end
